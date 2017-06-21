7aaede70c4e141ee5877ca4d1d28a62b4ae09247
lemon <lemon@localhost.localdomain>
2017-06-21 20:24:14 +0000

    now randomly generates a rudimentary map

--- ncjdr.py
+++ ncjdr.py
@@ -10,6 +10,11 @@ LIMIT_FPS = 20
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
 
+#Room values
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
 #Uses list comprehension
 def make_map():
 	global map
@@ -18,14 +23,58 @@ def make_map():
 		for y in range(MAP_HEIGHT) ]
 			for x in range(MAP_WIDTH) ]
 
-	room1 = Rect(20, 15, 10, 15)
-	room2 = Rect(20, 15, 10, 15)
-	create_room(room1)
-	create_room(room2)
-	player.x = 25
-	player.y = 23
-	create_h_tunnel(25, 55, 23)
-#	create_v_tunnel(25, 55, 23)
+	rooms = []
+	num_rooms = 0
+
+	for r in range(MAX_ROOMS): #Random width and height
+		w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		#Random position without going out of the boundaries of the map.
+		x = libtcod.random_get_int(0, 0, MAP_WIDTH - w -1)
+		y = libtcod.random_get_int(0, 0, MAP_HEIGHT -h -1)
+
+		#"Rect" class makes rectangles easier to work with
+		new_room = Rect(x, y, w, h)
+
+		#Run through room and see if it intersects with others
+		failed = False
+
+		for other_room in rooms:
+			if new_room.intersect(other_room):
+				failed = True
+				break
+		if not failed:
+			#This means there are no intersections, so this room is valid
+			#paint it to the map's tiles
+			create_room(new_room)
+			#center coordinates of new room, will be used later.
+			(new_x, new_y) = new_room.center()
+
+			if num_rooms == 0:
+				#This is the first room. Player starts here
+				player.x = new_x
+				player.y = new_y
+
+			else:
+				#all rooms after the first:
+				#connect it to the previous room with a tunnel
+
+				#center coordinates of the previous room
+				(prev_x, prev_y) = rooms[num_rooms -1].center()
+
+				#draw a coin (random number that is either 1 or 0)
+				if libtcod.random_get_int(0, 0, 1) == 1:
+					#first move horizontally, then vertically
+					create_h_tunnel(prev_x, new_x, prev_y)
+					create_v_tunnel(prev_y, new_y, prev_x)
+				else:
+					#first move vertically, then horizontally
+					create_v_tunnel(prev_y, new_y, prev_x)
+					create_h_tunnel(prev_x, new_x, prev_y)
+
+			#finally, append to the new room list
+			rooms.append(new_room)
+			num_rooms += 1
 
 #Create tunnels Vertically and Horizontally
 def create_h_tunnel(x1, x2, y):
@@ -84,6 +133,15 @@ class Rect:
 		self.x2 = x + w
 		self.y2 = y + h
 
+	def center(self):
+		center_x = (self.x1 + self.x2) / 2
+		center_y = (self.y1 + self.y2) / 2
+		return (center_x, center_y)
+
+	def intersect(self, other):
+		#returns true if this rectangle connects with another one
+		return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+			self.y1 <= other.y2 and self.y2 >= other.y1)
 
 #Map Tile Class
 class Tile:
