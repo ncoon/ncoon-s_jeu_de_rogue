052e91f71402215f97dde9dc579fd548cd9de35c
lemon <lemon@localhost.localdomain>
2017-06-23 18:21:57 +0000

    now shows character, though you can't walk through vertical tunnels

--- ncjdr.py
+++ ncjdr.py
@@ -1,12 +1,12 @@
 #Importing library and changing name to libtcod
 import libtcodpy as libtcod
 
+#Game window values
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
 #Map values
-
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
 
@@ -15,9 +15,109 @@ ROOM_MAX_SIZE = 10
 ROOM_MIN_SIZE = 6
 MAX_ROOMS = 30
 
+#Player's Field of View
+FOV_ALGO = 0 #defaulf FOV algorithm included in libtcod
+FOV_LIGHT_WALLS = True
+TORCH_RADIUS = 10
+
+#Walls/Ground coloring using libtcod's included library
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_light_wall = libtcod.Color(130, 110, 50)
+color_dark_ground = libtcod.Color(50, 50, 150)
+color_light_ground = libtcod.Color(200, 180, 50)
+
+#Create Rooms
+def create_room(room):
+	global map
+	#go through the tiles in rectangle and make them passable
+	for x in range(room.x1 + 1, room.x2):
+		for y in range(room.y1 + 1, room.y2): #Makes it so that two rooms will never overlap (Always have a space in between)
+			map[x][y].blocked = False
+			map[x][y].block_sight = False 
+
+#Rectangles on the map
+class Rect:
+	def __init__(self, x, y, w, h):
+		self.x1 = x
+		self.y1 = y
+		self.x2 = x + w
+		self.y2 = y + h
+
+	def center(self):
+		center_x = (self.x1 + self.x2) / 2
+		center_y = (self.y1 + self.y2) / 2
+		return (center_x, center_y)
+
+	def intersect(self, other):
+		#returns true if this rectangle connects with another one
+		return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+			self.y1 <= other.y2 and self.y2 >= other.y1)
+
+#Map Tile Class
+class Tile:
+	#a map tile and it's properties
+	def __init__(self, blocked, block_sight = None):
+		self.blocked = blocked
+		self.explored = False
+		if block_sight is None:
+			block_sight = blocked
+		self.block_sight = block_sight
+
+class Object:
+    #this is a generic object: the player, a monster, an item, the stairs...
+    #it's always represented by a character on screen.
+	def __init__(self, x, y, char, color):
+		self.x = x
+		self.y = y
+		self.char = char
+		self.color = color
+
+	def move(self, dx, dy):
+	#move by the given amount
+		if not map[self.x + dx][self.y + dy].blocked:
+			self.x += dx
+			self.y += dy
+
+	def draw(self):
+		#only show if it's visible to the player        
+		if libtcod.map_is_in_fov(fov_map, self.x, self.y):
+			#set the color and then draw the character that represents this object at its position
+			libtcod.console_set_default_foreground(con, self.color)
+			libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+	def clear(self):
+        #erase the character that represents this object #Clears the space where the character walks
+		libtcod.console_put_char(con, self.x, self.y, '.', libtcod.BKGND_NONE)
+
+def handle_keys():
+	global fov_recompute
+	key = libtcod.console_wait_for_keypress(True) #This line makes the game wait until the player presses a key. Rendering it turn-based.
+	#Checks for pressed keys and then changes the player coordinates on either 'x' or 'y' axis.
+	if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+		player.move(0, -1)
+		fov_recompute = True
+	elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+		player.move(0, 1)
+		fov_recompute = True
+	elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+		player.move(-1, 0)
+		fov_recompute = True
+	elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+		player.move(1, 0)
+		fov_recompute = True
+	#ALT+Enter: toggle fullscreen
+	if key.vk == libtcod.KEY_ENTER and key.lalt:
+		libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+	elif key.vk == libtcod.KEY_ESCAPE:
+		return True #exit game
+#TURN BASED GAMES USE console_check_for_keypress
+#Draw stuff before handling key input so that the game doesn't load with a blank screen.
+
+
 #Uses list comprehension
 def make_map():
-	global map
+	global map, player
 
 	map = [[ Tile(True)
 		for y in range(MAP_HEIGHT) ]
@@ -88,117 +188,45 @@ def create_v_tunnel(y1, y2, x):
 		map[x][y].block = False
 		map[x][y].block_sight = False
 
-
-#Walls
-color_dark_wall = libtcod.Color(0, 0, 100)
-color_dark_ground = libtcod.Color(50, 50, 150)
-
 #Renders Objects
 def render_all():
-	global color_dark_wall, color_light_wall
+	global fov_map, color_dark_wall, color_light_wall
 	global color_dark_ground, color_light_ground
+	global fov_recompute
 
-	for y in range(MAP_HEIGHT):
-		for x in range(MAP_WIDTH):
-			wall = map[x][y].block_sight
-			if wall:
-				libtcod.console_put_char_ex(con, x, y, '#', libtcod.white, libtcod.black)
-			else:
-				libtcod.console_put_char_ex(con, x, y, '.', libtcod.grey, libtcod.black )
+	if fov_recompute:
+		#recompute FOV if needed (player moved, etc)
+		fov_recompute = False
+		libtcod.map_compute_fov(fov_map, player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO)
+
+
+		for y in range(MAP_HEIGHT):
+			for x in range(MAP_WIDTH):
+				wall = map[x][y].block_sight
+				visible = libtcod.map_is_in_fov(fov_map, x, y)
+				if not visible:
+					#if not visible now, player can only see if it's explored
+					if map[x][y].explored:
+					#it's out of player's FOV
+						if wall:
+							libtcod.console_put_char_ex(con, x, y, '#', color_dark_wall, libtcod.black)
+						else:
+							libtcod.console_put_char_ex(con, x, y, '.', color_dark_ground, libtcod.black)
+				else:	
+					if wall: #it's visible
+						libtcod.console_put_char_ex(con, x, y, '#', color_light_wall, libtcod.black)
+					else:
+						libtcod.console_put_char_ex(con, x, y, '.', color_light_ground, libtcod.black )
+					map[x][y].explored = True #Thanks to this, explored regions are visible on screen,
+							#but in a different color than what is in FOV
+
 	#draw all objects in list (objects variable)
 	for object in objects:
-		object.draw()
+			object.draw()
 	#blit the contents of "con" to the root console
 	libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 
-#Clears the space where the character walks
-def clear(self):
-	if libtcod.map_is_in_fov(fov_map, self.x, self.y):
-		libtcod.console_put_char_ex(con, self.x, self.y, '.', libtcod.grey, libtcod.dark_blue)
-
-#Create Rooms
-def create_room(room):
-	global map
-	#go through the tiles in rectangle and make them passable
-	for x in range(room.x1 + 1, room.x2):
-		for y in range(room.y1 + 1, room.y2): #Makes it so that two rooms will never overlap (Always have a space in between)
-			map[x][y].blocked = False
-			map[x][y].block_sight = False 
-
-#Rectangles on the map
-class Rect:
-	def __init__(self, x, y, w, h):
-		self.x1 = x
-		self.y1 = y
-		self.x2 = x + w
-		self.y2 = y + h
-
-	def center(self):
-		center_x = (self.x1 + self.x2) / 2
-		center_y = (self.y1 + self.y2) / 2
-		return (center_x, center_y)
-
-	def intersect(self, other):
-		#returns true if this rectangle connects with another one
-		return (self.x1 <= other.x2 and self.x2 >= other.x1 and
-			self.y1 <= other.y2 and self.y2 >= other.y1)
-
-#Map Tile Class
-class Tile:
-	#a map tile and it's properties
-	def __init__(self, blocked, block_sight = None):
-		self.blocked = blocked
-
-		if block_sight is None:
-			block_sight = blocked
-		self.block_sight = block_sight
-
-class Object:
-    #this is a generic object: the player, a monster, an item, the stairs...
-    #it's always represented by a character on screen.
-	def __init__(self, x, y, char, color):
-		self.x = x
-		self.y = y
-		self.char = char
-		self.color = color
-
-	def move(self, dx, dy):
-	#move by the given amount
-		if not map[self.x + dx][self.y + dy].blocked:
-			self.x += dx
-			self.y += dy
-
-	def draw(self):
-        #set the color and then draw the character that represents this object at its position
-		libtcod.console_set_default_foreground(con, self.color)
-		libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
-
-	def clear(self):
-        #erase the character that represents this object
-		libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
-
-def handle_keys():
-	key = libtcod.console_wait_for_keypress(True) #This line makes the game wait until the player presses a key. Rendering it turn-based.
-	#Checks for pressed keys and then changes the player coordinates on either 'x' or 'y' axis.
-	if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-		player.move(0, -1)
-	elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-		player.move(0, 1)
-	elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-		player.move(-1, 0)
-	elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-		player.move(1, 0)
-	#ALT+Enter: toggle fullscreen
-	if key.vk == libtcod.KEY_ENTER and key.lalt:
-		libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
-
-	elif key.vk == libtcod.KEY_ESCAPE:
-		return True #exit game
-
-#TURN BASED GAMES USE console_check_for_keypress
-#Draw stuff before handling key input so that the game doesn't load with a blank screen.
-
-####Initialization
+####Initialization############################
 
 
 player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
@@ -213,6 +241,15 @@ con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT,) #Creates another off-scr
 
 #Call make_map function
 make_map()
+
+#Tells libtcod's FOV module which tiles block sight
+fov_map = libtcod.map_new(MAP_WIDTH, MAP_HEIGHT)
+for y in range(MAP_HEIGHT):
+	for x in range(MAP_WIDTH):
+		libtcod.map_set_properties(fov_map, x, y, not map[x][y].block_sight, not map[x][y].blocked)
+
+fov_recompute = True
+
 #MAIN LOOP - Keeps running game logic so long as window is not closed.
 
 while not libtcod.console_is_window_closed():
